1225dd8f0c4d5bfaae3611340c285e693556d516
bgosavi <bgosavi@qualys.com>
2021-02-17 15:49:35 +0530

    formatting files

--- moviebuzz-api/src/main/java/com/moviebuzz/api/MoviebuzzApiApplication.java
+++ moviebuzz-api/src/main/java/com/moviebuzz/api/MoviebuzzApiApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoviebuzzApiApplication {
+public class MoviebuzzApiApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviebuzzApiApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MoviebuzzApiApplication.class, args);
+    }
 
 }
